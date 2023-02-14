f2e5b5e673157640775a5bacb818b077d19240c7
VirbickasGytautas <guzbarahor@gmail.com>
2023-02-14 17:39:27 +0300

    Upd, отработка телегамм ботом

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import telebot
-from datetime import datetime
-import datetime
-from telebot import types
-from collections import defaultdict
-#import xlrd не подходит
 import csv
+import telebot
+from telebot import types
+import datetime
+from datetime import datetime
+import time
+import io
+import pandas as pd
 
+start_time = time.time()
 bot = telebot.TeleBot("5941676589:AAExTrhG3aZCkG13obkHyzPE-Z8F5NTxq_A") #API телеграмм бота
 
 @bot.message_handler(commands=['start'])
@@ -14,8 +16,8 @@ def send_welcome(message):
 
 	#Меню, клавиатура
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-	but1 = types.KeyboardButton("Статистика со стенда ДСТ УУР")
-	but2 = types.KeyboardButton("Статистика со стенда Настройки БУ БиД")
+	but1 = types.KeyboardButton("Статистика УУР")
+	but2 = types.KeyboardButton("Отчеты по качеству")
 	markup.add(but1, but2)
 
 	bot.reply_to(message, "Здравствуй, {0.first_name}\nБот в режиме разработки, многие функции недоступны, "
@@ -26,35 +28,66 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def menu(message):
 	if message.chat.type == 'private':
-		if message.text == "Статистика со стенда ДСТ УУР":
-			with open("tmpID.csv", encoding='utf-8') as r_file:
-				values = defaultdict(int)
-				file_reader = csv.reader(r_file, delimiter=";")
-				count = 0
-				for row in file_reader:
-					count == 0
-					count += 1
-					#Подсчет строк
-					#if count == 0:
-					#	print(f'Файл содержит столбцы: {", ".join(row)}')
-					#else:
-					#	print(f'{row[3]} - {row[0]} - {row[1]} - {row[2]} {row[4]}')
-					#count += 1
-				dateNOWstr = datetime.datetime.now()
+		if message.text == "Статистика УУР":
+			cid = message.chat.id
+			msgSD = bot.send_message(cid,'Введите дату начала периода в формате дд.мм.гггг Ч:М:С (Пример 24.01.2022 07:00:00):')
+			bot.register_next_step_handler(msgSD, stepSD)
 
-			bot.send_message(message.chat.id, dateNOWstr)
-			bot.send_message(message.chat.id, count)
-		elif message.text == "Статистика со стенда Настройки БУ БиД":
+		elif message.text == "Отчеты по качеству":
 			#инлайновая клавиатура
 			inMurkup = types.InlineKeyboardMarkup(row_width=1)
-			but1 = types.InlineKeyboardButton("Умная кнопка 1", callback_data='book1')
-			but2 = types.InlineKeyboardButton("Умная кнопка 2", callback_data='book2')
-			but3 = types.InlineKeyboardButton("Умная кнопка 3", callback_data='book3')
-			but4 = types.InlineKeyboardButton("Умная кнопка 4", callback_data='book4')
+			but1 = types.InlineKeyboardButton("ДЕФЕКТЫ НА ПРОИЗВОДСТВЕ ЭУР 2022 год", callback_data='Отчет1')
+			but2 = types.InlineKeyboardButton("Умная кнопка 2", callback_data='Отчет2')
+			but3 = types.InlineKeyboardButton("Умная кнопка 3", callback_data='Отчет3')
+			but4 = types.InlineKeyboardButton("Умная кнопка 4", callback_data='Отчет4')
 			inMurkup.add(but1, but2, but3, but4)
 			bot.send_message(message.chat.id, "ВОТ СПИСОК УМНЫХ НЕ РАБОЧИХ КНОПОК", reply_markup=inMurkup)
 		else:
 			bot.send_message(message.chat.id, "Я не знаю что и ответить")
+def stepSD(message):
+	cid = message.chat.id
+	global uSD
+	uSD = message.text
+	uED = str
+	print(uSD)
+	bot.send_message(cid, "Сохранил!")
+	msgED = bot.send_message(cid,'Введите дату конца периода в формате дд.мм.гггг Ч:М:С (Пример 24.01.2022 07:00:00):')
+	bot.register_next_step_handler(msgED, stepED)
+
+def stepED(message):
+	cid = message.chat.id
+	global uED
+	uED = message.text
+	uSD = str
+	print(uED)
+	bot.send_message(cid, "Сохранил!")
+	msgRD = bot.send_message(cid, "Подождите около минуты")
+	resultRD(message)
+
+def resultRD(message):
+	cid = message.chat.id
+	print(uSD,uED)
+	file = 'tmpID.csv'
+	dateparse = lambda x: pd.datetime.strptime(x, "%d.%m.%Y %H:%M:%S")
+	dateparse = str
+	today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+	df = pd.read_csv(file, index_col=0, sep=';', names=['1', '2', '3', '4', '5'], on_bad_lines='skip', parse_dates=[4], date_parser=dateparse)
+	df['4'] = pd.to_datetime(df['4'])
+	newdf = (df['4'] > uSD) & (df['4'] <= uED)
+	newdf = df.loc[newdf]
+	newdf = len(newdf.index)
+	total = (df['4'] > '11.01.2023 00:00:00') & (df['4'] <= datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+	total = len(total.index)
+
+	print('Сегодня:', today)
+	print('Количество произведенных за выбранный период: ',newdf)
+	print('За 2023 год:', total )
+	print("Операция выполнена за %s секунд" % (time.time() - start_time))
+
+	bot.send_message(cid, text = "Сегодня:")
+	bot.send_message(cid,today)
+	bot.send_message(cid,"Количество произведенных за выбранный период: ")
+	bot.send_message(cid, newdf)
 
 #обработка callback
 @bot.callback_query_handler(func=lambda call: True)
@@ -62,16 +95,16 @@ def callback_inline(call):
 	try:
 		if call.message:
 			if call.data == 'Отчет1':
-				doc = open('boo/Файл1.pdf', 'rb')
+				doc = open('report/Файл1.pdf', 'rb')
 				bot.send_document(call.message.chat.id, doc)
 			elif call.data == 'Отчет2':
-				doc = open('boo/boo/Файл2.pdf','rb')
+				doc = open('report/Файл2.pdf','rb')
 				bot.send_document(call.message.chat.id, doc)
 			elif call.data == 'Отчет3':
-				doc = open('boo/Файл3.pdf', 'rb')
+				doc = open('report/Файл3.pdf', 'rb')
 				bot.send_document(call.message.chat.id, doc)
 			elif call.data == 'Отчет4':
-				doc = open('boo/Файл4.pdf', 'rb')
+				doc = open('report/Файл4.pdf', 'rb')
 				bot.send_document(call.message.chat.id, doc)
 			#удаляем инлайновую клаву
 			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Форматы отчетов в разработке",
--- test.py
+++ test.py
@@ -1,16 +1,36 @@
-import pandas as pd
 import csv
-import datetime
+import time
+import pandas as pd
+import io
+from datetime import datetime
 
-#start_date = datetime.strptime(input('Введите дату в формате дд.мм.гггг: '), '%d.%m.%Y')
 
-with open("tmpID.csv", encoding='utf-8') as r_file:
-    file_reader = csv.reader(r_file, delimiter = ";")
-    count = 0
+pd.set_option('display.width', 1400)
+pd.set_option('display.max_columns', 5)
+start_time = time.time()
 
-    for row in file_reader:
-        count == 0
-        count += 1
-    datetime_object = datetime.datetime.now()
-    print(datetime_object)
-    print(f'Всего в файле {count} строк.')
+#start_date = input('Введите дату начала периода в формате дд.мм.гггг Ч:М:С (Пример 24.01.2022 07:00:00): ')
+#end_date = input('Введите дату конца периода в формате дд.мм.гггг Ч:М:С (Пример 24.01.2022 07:00:00): ')
+
+#with open("tmpID.csv", encoding='utf-8') as r_file:
+#    file_reader = csv.reader(r_file, delimiter = ";")
+#    count = sum(1 for row in file_reader)
+
+#print(f'Всего в файле {count} строк.')
+file = 'tmpID.csv'
+today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+df = pd.read_csv(file, index_col=0, sep=';', names=['1', '2', '3', '4', '5'], on_bad_lines='skip')
+df['4'] = pd.to_datetime(df['4'])
+newdf = (df['4'] > '13.02.2023 00:00:00') & (df['4'] <= '14.02.2023 00:00:00')
+newdf = df.loc[newdf]
+total = (df['4'] > '11.01.2023 00:00:00') & (df['4'] <= datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+
+print('Сегодня:', today)
+print('Количество произведенных за выбранный период: ',len(newdf.index))
+print('За 2023 год:', len(total.index))
+#print(df)
+
+
+print("Операция выполнена за %s секунд" % (time.time() - start_time))
+
+
