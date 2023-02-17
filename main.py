971f7fc155dd7c0fc4f2755fd17a4b275565e8b1
VirbickasGytautas <guzbarahor@gmail.com>
2023-02-17 16:29:25 +0300

    Upd, добавлен белый список, обработка помощи

--- main.py
+++ main.py
@@ -11,29 +11,62 @@ today = datetime.today().strftime('%Y.%m.%d %H:%M:%S')
 file = r'\\pk-55\CSV\tmpID.csv' #статистика с ДСП
 
 #Белый список
-list('Белый список')
-[415077278,565955,5955564]
+list = [415077278,376187604,905566669]
 
-#bot.send_message(message.chat.id, 'Доступ ограничен')
+#dev - 415077278
+#Илья Билокур - 376187604
+#Дмитрий Ульянов - 905566669
+
+#TODO
+#Ограничить доступ по белому списку
+#Создать регистрацию и введение данных в СУБД
+#Обработка CSV по парт-номерам
+#Обработка нескольких CSV
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def check_user(message):
 	userID = message.chat.id
 	userName = message.chat.first_name
-	print('id пользователя:', userID)
-	print('Имя пользователя: ', userName)
-	stic = open('stic/welcome.webp', 'rb') #стикер
+	print(userName, userID)
+	if userID in list:
+		print('Авторизация', userID, userName)
+		send_welcome(message)
+	else:
+		chatid = 415077278
+		userID = message.chat.id
+		userName = message.chat.first_name
+		bot.forward_message(chatid, message.chat.id, message.message_id, userID, userName)
+		bot.send_message(message.chat.id, 'Доступ ограничен')
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		but1 = types.KeyboardButton("Помощь")
+		markup.add(but1)
+		bot.reply_to(message, "Здравствуй, {0.first_name}\nДля авторизации обратитесь к разработчику"
+					 .format(message.from_user), parse_mode='html', reply_markup=markup)
 
-	#Меню, клавиатура
+def send_welcome(message):
+	stic = open('stic/welcome.webp', 'rb')  # стикер
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	but1 = types.KeyboardButton("Статистика УУР")
 	but2 = types.KeyboardButton("Отчеты по качеству")
-	markup.add(but1, but2)
+	but3 = types.KeyboardButton("Помощь")
+	markup.add(but1, but2, but3)
 	bot.reply_to(message, "Здравствуй, {0.first_name}\nБот в режиме разработки, многие функции недоступны"
-				 .format(message.from_user),parse_mode='html',reply_markup=markup)
-	bot.send_sticker(message.chat.id,stic)
+				 .format(message.from_user), parse_mode='html', reply_markup=markup)
+	bot.send_sticker(message.chat.id, stic)
 
-#Ответ бота на определенные команды
+@bot.message_handler(commands=['help'])
+def help(message):
+	cid2 = message.chat.id
+	stic = open('stic/help.webp', 'rb')
+	bot.send_sticker(message.chat.id, stic)
+	help_message = bot.send_message(cid2, 'Пожалуйста, опишите вашу проблему:')
+	bot.register_next_step_handler(help_message, message_to_help)
+
+def message_to_help(message):
+	chatid = 415077278
+	help_message = bot.send_message(message.chat.id, 'Ваше сообщение отправлено разработчику')
+	bot.forward_message(chatid, message.chat.id, message.message_id)
+
 @bot.message_handler(func=lambda message: True)
 def menu(message):
 	if message.chat.type == 'private':
@@ -54,7 +87,9 @@ def menu(message):
 			but3 = types.InlineKeyboardButton("Умная кнопка 3", callback_data='Отчет3')
 			but4 = types.InlineKeyboardButton("Умная кнопка 4", callback_data='Отчет4')
 			inMurkup.add(but1, but2, but3, but4)
-			bot.send_message(message.chat.id, "ВОТ СПИСОК УМНЫХ НЕ РАБОЧИХ КНОПОК", reply_markup=inMurkup)
+			bot.send_message(message.chat.id, "Умные, не рабочие кнопки", reply_markup=inMurkup)
+		elif message.text == "Помощь":
+			help(message)
 		else:
 			bot.send_message(message.chat.id, "Я не знаю что и ответить")
 def stepSD(message):
@@ -64,9 +99,15 @@ def stepSD(message):
 	uED = str
 	print(uSD)
 	bot.send_message(cid, "Сохранил!")
-	msgED = bot.send_message(cid,'Введите дату конца периода в формате гггг.мм.дд Ч:М:С\n'
-								 '(Пример 2023.01.02 07:00:00):')
+	msgED = bot.send_message(cid,'Введите дату *КОНЦА* периода в формате гггг.мм.дд Ч:М:С\n'
+								 '(Пример 2023.01.02 07:00:00):', parse_mode= 'Markdown')
 	bot.register_next_step_handler(msgED, stepED)
+
+def period(message):
+	cid = message.chat.id
+	msgSD = bot.send_message(cid, 'Введите дату *НАЧАЛА* периода в формате гггг.мм.дд Ч:М:С\n'
+								  '(Пример 2023.02.17 07:00:00):', parse_mode= 'Markdown')
+	bot.register_next_step_handler(msgSD, stepSD)
 
 def stepED(message):
 	cid = message.chat.id
@@ -79,34 +120,35 @@ def stepED(message):
 	resultRD(message)
 
 def resultRD(message):
-	cid = message.chat.id
-	print(uSD,uED)
-	df = pd.read_csv(file, index_col=0, sep=';', names=['1', '2', '3', '4', '5'], on_bad_lines='skip')
-	df['4'] = pd.to_datetime(df['4'], format='%d.%m.%Y %H:%M:%S')
+	try:
+		stic = open('stic/error.webp', 'rb')
+		cid = message.chat.id
+		print('С', uSD, 'По', uED)
+		df = pd.read_csv(file, index_col=0, sep=';', names=['1', '2', '3', '4', '5'], on_bad_lines='skip')
+		df['4'] = pd.to_datetime(df['4'], format='%d.%m.%Y %H:%M:%S')
 
-	newdf = (df['4'] >= uSD) & (df['4'] <= uED)
-	newdf = df.loc[newdf]
-	newdf = len(newdf.index)
-	total = (df['4'] > '2023.01.01 00:00:00') & (df['4'] <= today)
-	total = len(total.index)
+		newdf = (df['4'] >= uSD) & (df['4'] <= uED)
+		newdf = df.loc[newdf]
+		newdf = len(newdf.index)
+		total = (df['4'] > '2023.01.01 00:00:00') & (df['4'] <= today)
+		total = len(total.index)
 
-	print('Запрос от:', str(message.chat.first_name), 'ID:',str(message.chat.id))
-	print('Сегодня:', today)
-	print('Количество произведенных за выбранный период: ',newdf)
-	print('За 2023 год:', total )
-	print("Операция выполнена за %s секунд" % (time.time() - start_time))
+		print('Запрос от:', str(message.chat.first_name), 'ID:',str(message.chat.id))
+		print('Сегодня:', today)
+		print('Количество произведенных за выбранный период: ',newdf)
+		print('За 2023 год:', total )
+		print("Операция выполнена за %s секунд" % (time.time() - start_time))
 
-	#bot.send_message(cid, "Сегодня:")
-	#bot.send_message(cid,today)
-	bot.send_message(cid,f'Количество произведенных за выбранный период: \n'
-						 f'С {uSD} по {uED}')
-	bot.send_message(cid, newdf)
-
-def period(message):
-	cid = message.chat.id
-	msgSD = bot.send_message(cid, 'Введите дату начала периода в формате гггг.мм.дд Ч:М:С\n'
-								  '(Пример 2023.02.17 07:00:00):')
-	bot.register_next_step_handler(msgSD, stepSD)
+		#bot.send_message(cid, "Сегодня:")
+		#bot.send_message(cid,today)
+		bot.send_message(cid,f'Количество произведенных за выбранный период: \n'
+							 f'С {uSD} по {uED}')
+		bot.send_message(cid, newdf)
+	except:
+		print('Ошибка ввода данных')
+		bot.send_message(cid, 'Ошибка ввода данных')
+		bot.send_message(cid, 'Возможно вы ввели некорректно даты периодов, попробуйте снова')
+		bot.send_sticker(cid, stic)
 
 def callback_today(message):
 	cid = message.chat.id
